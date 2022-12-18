94b57f85606f2cb836af21b4ed3220da043eeaac
Victor <vicharmonic@gmail.com>
2022-12-18 21:33:00 +0000

    remove an unnecessary comment

--- webmap.py
+++ webmap.py
@@ -6,7 +6,7 @@ import pandas as pd
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles='cartodb dark_matter')
 
 # map.add_child(folium.Marker(location=[38.2, -99.1], popup="I am a Marker", icon=folium.Icon(color="green")))
-# or( a better way to add children)
+
 fgv = folium.FeatureGroup(name="Volcanoes")
 
 # features = [[38.2, -99.1], [39.2, -97.1]]
